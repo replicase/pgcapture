9f15eeb77763f0117a5e16f2de654d2346232279
benjamin99 <cknomber1@gmail.com>
2023-02-22 00:29:06 +0800

    test(cursor): add TestPulsarTracker

--- pkg/cursor/pulsar_test.go
+++ pkg/cursor/pulsar_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/apache/pulsar-client-go/pulsar"
 )
 
-func newPulsarSubscriptionTracker(topic string) (*PulsarSubscriptionTracker, func(), error) {
+func newPulsarTracker(topic string) (*PulsarTracker, func(), error) {
 	client, err := pulsar.NewClient(pulsar.ClientOptions{
 		URL: "pulsar://localhost:6650",
 	})
@@ -17,23 +17,23 @@ func newPulsarSubscriptionTracker(topic string) (*PulsarSubscriptionTracker, fun
 		return nil, nil, err
 	}
 
-	tracker, err := NewPulsarSubscriptionTracker(client, topic)
-	if err != nil {
-		client.Close()
-		return nil, nil, err
+	tracker := &PulsarTracker{
+		Client:      client,
+		PulsarTopic: topic,
 	}
 
 	closeFunc := func() {
 		tracker.Close()
 		client.Close()
 	}
+
 	return tracker, closeFunc, nil
 }
 
-func TestPulsarSubscriptionTracker(t *testing.T) {
+func TestPulsarTracker(t *testing.T) {
 	topic := time.Now().Format("20060102150405")
 
-	tracker, cancel, err := newPulsarSubscriptionTracker(topic)
+	tracker, cancel, err := newPulsarTracker(topic)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -59,27 +59,21 @@ func TestPulsarSubscriptionTracker(t *testing.T) {
 		_ = producer.Flush()
 	}()
 
-	var cp Checkpoint
-	var mid pulsar.MessageID
 	for i := 0; i < 10; i++ {
-		cp = Checkpoint{LSN: uint64(i + 100)}
-		mid, err = producer.Send(context.Background(), &pulsar.ProducerMessage{
+		cp := Checkpoint{LSN: uint64(100 + i)}
+		if _, err := producer.Send(context.Background(), &pulsar.ProducerMessage{
 			Key:     cp.ToKey(),
 			Payload: []byte("test-" + strconv.Itoa(i)),
-		})
-		if err != nil {
+		}); err != nil {
 			t.Fatal(err)
 		}
-	}
-
-	if err := tracker.Commit(cp, mid); err != nil {
-		t.Fatal(err)
 	}
 
 	last, err := tracker.Last()
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	if last.LSN != 109 {
 		t.Fatalf("unexpected checkpoint.LSN: %v", last.LSN)
 	}
