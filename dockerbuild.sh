e72e0fe50047989d2484a81968707b52ded942e0
Kenny Chen <kennychen851228@gmail.com>
2023-03-12 15:37:26 +0800

    feat: support build multi arch image

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 SHA=$(git rev-parse --short HEAD)
+VERSION=$(git describe --tags)
+PLATFORM=${1:-linux/amd64}
 
-echo "building rueian/pgcapture:$SHA"
+echo "building rueian/pgcapture:$VERSION in $PLATFORM platform"
 
-docker build -t rueian/pgcapture:$SHA --build-arg SHA=$SHA .
+docker buildx build --platform $PLATFORM --build-arg SHA=$SHA  --build-arg VERSION=$VERSION -t rueian/pgcapture:latest -t rueian/pgcapture:$VERSION .
