9f081521ef9a2efce21d17978e06843e92f578e7
Kenny Chen <kennychen851228@gmail.com>
2023-08-03 17:00:12 +0800

    chore: add build and codegen script

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,5 @@
-FROM golang:1.20-bullseye AS base
-
-WORKDIR /src
-
-ADD go.* .
-RUN go mod download
-
-ADD . .
-
-ARG SHA
-ARG VERSION
-RUN go build -ldflags="-X github.com/rueian/pgcapture/cmd.CommitSHA=${SHA} -X github.com/rueian/pgcapture/cmd.Version=${VERSION}" -x -o pgcapture main.go
-
 FROM gcr.io/distroless/base-debian10
 
-COPY --from=base /src/pgcapture /pgcapture
+COPY bin/out /
 
 ENTRYPOINT ["/pgcapture"]
--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,13 +5,22 @@ set -e
 export LOCAL_GOPATH=$(echo "${GOPATH:-~/go}" | cut -d ':' -f 1) # take the first GOPATH to mount into docker compose
 export PLATFORM=${PLATFORM:-linux/amd64}
 export PG_VERSION=${PG_VERSION:-14}
+export PGCAPTURE_SHA=$(git rev-parse --short HEAD)
+export PGCAPTURE_VERSION=$(git describe --tags --abbrev=0)
 
 [ ! -d "$LOCAL_GOPATH/pkg/mod/cache" ] && mkdir -p "$LOCAL_GOPATH/pkg/mod/cache"
 
 case "$1" in
+  build)
+    docker-compose run --rm build
+    docker-compose build --force-rm pgcapture
+    ;;
   test)
     docker-compose run --rm test-deps
     docker-compose run --rm test
+    ;;
+  codegen)
+    docker-compose run --rm codegen
     ;;
   clean)
     docker-compose down
